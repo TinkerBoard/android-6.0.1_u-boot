882e3613e8787b9662b158d9cdefc447b3e74517
cwz <cwz@rock-chips.com>
2016-03-18 10:09:15 +0800

    rk312x: fix some emmc work error.
    1. fix emmc clock source error.
    2. reduce ahp clock rate to 100M.
    
    Change-Id: I35cfce9849ce03fc3d9269ac18943ead73389f8f
    Signed-off-by: cwz <cwz@rock-chips.com>

--- arch/arm/cpu/armv7/rk32xx/clock-rk312X.c
+++ arch/arm/cpu/armv7/rk32xx/clock-rk312X.c
@@ -121,7 +121,7 @@ static struct pll_clk_set gpll_clks[] = {
 	 * _mhz, _refdiv, _fbdiv, _postdiv1, _postdiv2, _dsmpd, _frac,
 	 *	aclk_div, hclk_div, pclk_div
 	 */
-	_GPLL_SET_CLKS(594000, 2, 99, 2, 1, 1, 0,	4, 2, 2),
+	_GPLL_SET_CLKS(594000, 2, 99, 2, 1, 1, 0,	6, 2, 2),
 	_GPLL_SET_CLKS(297000, 2, 99, 4, 1, 1, 0,	2, 2, 2),
 };
 
@@ -972,7 +972,8 @@ uint32 rkclk_get_mmc_freq_from_gpll(uint32 sdid)
 	/* set general pll */
 	rkclk_set_mmc_clk_src(sdid, 1);
 
-	return rkclk_get_mmc_clk(sdid);
+	/* emmc automic divide freq to 1/2, so here divide freq to 1/2 */
+	return rkclk_get_mmc_clk(sdid) / 2;
 }
 
 
